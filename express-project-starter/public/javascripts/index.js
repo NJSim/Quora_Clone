9d6008cddc9e286bf36f78fe723429841f81f0e8
Kristy Zhang <zhangjy1988@gmail.com>
2021-09-21 23:57:08 -0400

    resolve logout

--- express-project-starter/public/javascripts/index.js
+++ express-project-starter/public/javascripts/index.js
@@ -1,47 +1,48 @@
-document.addEventListener("DOMContentLoaded", e=>{
-    const answer = document.getElementById('answer');
-    answer.addEventListener('click', e=>{
-        const answerContain = document.getElementById('answerContain');
-        answerContain.style.display = "block";
-    })
+document.addEventListener("DOMContentLoaded", (e) => {
+  const answer = document.getElementById("answer");
+  if (answer) {
+    answer.addEventListener("click", (e) => {
+      const answerContain = document.getElementById("answerContain");
+      answerContain.style.display = "block";
+    });
+  }
 
+  const profile = document.getElementById("profileSelect");
+  if (profile) {
+    profile.addEventListener("change", async (e) => {
+      if (e.target.value === "myQuestion") {
+      } else if (e.target.value === "myAnswer") {
+      } else {
+        await fetch("http://localhost:8080/users/logout", {
+          method: "POST",
+        }).then((response) => {
+          if (response.redirected) {
+            window.location.href = response.url;
+          }
+        });
+      }
+    });
+  }
 
-    const profile = document.getElementById('profileSelect');
-    profile.addEventListener('change', async(e)=>{
-        console.log(profile);
-        if(profile.value === "myQuestion"){
-            
-        }
-        else if(profile.value === "myAnswer"){
-        
-        }
-        else{
-            await fetch("localhost://8080/users/logout", {
-            method: "POST"});
-        }
-    }
-    )
-});
-
-const vote = document.querySelectorAll('.upvote-button');
+  const vote = document.querySelectorAll(".upvote-button");
 
   for (let i = 0; i < vote.length; i++) {
-    vote[i].addEventListener('click', event => {
-      const userid = document.getElementById('userid').value;
-      const questionid = document.getElementById(`questionid`).id;
-      
-      console.log('userid', userid)
-      console.log('questionid', questionid)
-    
-  
-    // const vote = await db.Questions_vote.findOne({
-    //   where: { user_id }
-    // });
+    vote[i].addEventListener("click", (event) => {
+      const userid = document.getElementById("userid").value;
+      const questionid = document.getElementsByClassName("upvote-button")[i].id;
 
-    //check database questions_votes has user's vote
+      console.log("userid", userid);
+      console.log("questionid", questionid);
+
+      // const vote = await db.Questions_vote.findOne({
+      //   where: { user_id }
+      // });
+
+      //check database questions_votes has user's vote
       //if it does
-        //delete the vote
+      //delete the vote
       //else
-        //add the vote to the questions_vote database table
-  })
-};
+      //add the vote to the questions_vote database table
+    });
+  }
+});
